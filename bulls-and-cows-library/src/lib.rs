2849ea9dd9b908338bd9a9d86f4611a2e188071a
Oliver Forral <intrepion@gmail.com>
2022-02-20 22:54:17 -0700

    red

--- bulls-and-cows-library/src/lib.rs
+++ bulls-and-cows-library/src/lib.rs
@@ -1,8 +1,10 @@
 #[cfg(test)]
-mod tests {
+mod count_cattle_should {
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn return_4_bulls_and_0_cows_when_exactly_correct() {
+        let secret_code = "1234";
+        let guess = "1234";
+
+        let actual = count_cattle(secret_code, guess);
     }
 }
