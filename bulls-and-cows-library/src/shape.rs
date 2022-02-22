feb5aef63f6ce4b831c9eb13e81604912ae87e04
Oliver Forral <intrepion@gmail.com>
2022-02-22 15:38:34 -0700

    blue

--- bulls-and-cows-library/src/shape.rs
+++ bulls-and-cows-library/src/shape.rs
@@ -1,3 +1,6 @@
+use rand::distributions::{Distribution, Standard};
+use rand::Rng;
+
 #[derive(Clone, Debug, PartialEq)]
 pub enum Shape {
     Circle,
@@ -8,4 +11,19 @@ pub enum Shape {
     Square,
     Spade,
     Triangle,
+}
+
+impl Distribution<Shape> for Standard {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Shape {
+        match rng.gen_range(0..=7) {
+            0 => Shape::Circle,
+            1 => Shape::Club,
+            2 => Shape::Diamond,
+            3 => Shape::Heart,
+            4 => Shape::Star,
+            5 => Shape::Square,
+            6 => Shape::Spade,
+            _ => Shape::Triangle,
+        }
+    }
 }
